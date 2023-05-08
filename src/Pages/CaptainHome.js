a416228fdb868fbf28d3611fc1e857b6e5edae74
sasunnethmina <sasunnethmina@gmail.com>
2023-05-08 17:05:11 +0530

    qwer

--- src/Pages/CaptainHome.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CaptainHome() {
-  return <div>CaptainHome</div>;
-}
-
-export default CaptainHome;
--- src/Pages/MemberHome.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function MemberHome() {
-  return <div>MemberHome</div>;
-}
-
-export default MemberHome;
